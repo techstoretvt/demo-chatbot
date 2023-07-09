335268668e706c5b881197f74719239dc30ca6d0
techstoretvt <techstoretvt2565@gmail.com>
2023-07-09 15:42:07 +0700

    sfsdf

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,54 +1,31 @@
 require('dotenv').config();
 import nodemailer from 'nodemailer'
+import request from 'request';
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS_EMAIL,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    }
+});
+
 let getHomePage = (req, res) => {
-    // let transporter = nodemailer.createTransport({
-    //     host: "smtp.gmail.com",
-    //     port: 587,
-    //     secure: false,
-    //     auth: {
-    //         user: process.env.EMAIL,
-    //         pass: process.env.PASS_EMAIL,
-    //     },
-    //     tls: {
-    //         rejectUnauthorized: false,
-    //     }
-    // });
 
-
-    // let info = transporter.sendMail({
-    //     from: `"TechStoreTvT ⚔ ⚓ 👻" <${process.env.EMAIL}>`,
-    //     to: 'ngoantung2565@gmail.com',
-    //     subject: 'log demo chatbot' + 'sfsdf',
-    //     html: 'sender_psid',
-    // });
     return res.send('Hello home page');
 }
 
 let postWebHook = (req, res) => {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS_EMAIL,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        }
-    });
 
-
-    let info = transporter.sendMail({
-        from: `"TechStoreTvT ⚔ ⚓ 👻" <${process.env.EMAIL}>`,
-        to: 'ngoantung2565@gmail.com',
-        subject: 'log demo chatbot' + 'sfsdf',
-        html: 'vao route post webhook',
-    });
     let body = req.body;
 
     if (body.object === "page") {
@@ -60,29 +37,33 @@ let postWebHook = (req, res) => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
+
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
 
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
 
-            let transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 587,
-                secure: false,
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.PASS_EMAIL,
-                },
-                tls: {
-                    rejectUnauthorized: false,
-                }
-            });
+
+
+
+
+
+
+            //send email log
+
 
 
             let info = transporter.sendMail({
                 from: `"TechStoreTvT ⚔ ⚓ 👻" <${process.env.EMAIL}>`,
                 to: 'ngoantung2565@gmail.com',
-                subject: 'log demo chatbot' + 'sfsdf',
+                subject: 'log demo chatbot' + webhook_event.message.text,
                 html: 'sender_psid ' + sender_psid,
             });
 
@@ -141,7 +122,19 @@ let getWebHook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -151,7 +144,33 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+            let info = transporter.sendMail({
+                from: `"TechStoreTvT ⚔ ⚓ 👻" <${process.env.EMAIL}>`,
+                to: 'ngoantung2565@gmail.com',
+                subject: 'Gui thanh cong' + response.text,
+                html: 'Gui thanh cong toi: ' + sender_psid,
+            });
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
 
 
